e0ac174a0f37784949195c3bc2222b46ba51a492
savin9770 <105098438+savin9770@users.noreply.github.com>
2022-12-08 22:32:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(path.join(__dirname+'/home.html'));
   //__dirname : It will resolve to your project folder.
 });
 
